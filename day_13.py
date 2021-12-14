e7670ba6accbaeac1a238b47818cda5af96c9819
Jakub Krech <kuba.kre@o2.pl>
2021-12-14 02:16:23 +0100

    Improvement for day_13

--- day_13.py
+++ day_13.py
@@ -1,40 +1,31 @@
 from utils.timer import timer_decorator
 
 @timer_decorator
-def FoldThePaper(points, folds, max_x, max_y):
-
-    paper_sheet = [['.' for x in range(max_y + 1)] for y in range(max_x + 1)]
+def FoldThePaper(points, folds, x1, x2, y1, y2):
+    paper_sheet = [['.' for x in range(y2)] for y in range(x2)]
     first_fold_dot_count = None
 
     for point in points:
         paper_sheet[point[0]][point[1]] = '#'
 
     for fold in folds:
-        if 'x' in fold[0]:
-            new_paper = [['.' for x in range(len(paper_sheet[0]))] for y in range(int(len(paper_sheet) / 2))]
-            
-            for x in range(len(paper_sheet)):
-                new_x = abs(x - fold[1]) - 1
-                for y in range(len(paper_sheet[0])):
+        if 'x' == fold[0]:
+            for x in range(fold[1] + 1, x2):
+                for y in range(y1, y2):
                     if paper_sheet[x][y] == '#':
-                        new_paper[new_x][y] = '#'
-
-            paper_sheet = new_paper[:]
+                        paper_sheet[x2 - x - 1][y] = '#'
+            x2 = fold[1]
         else:
-            new_paper = [['.' for x in range(int(len(paper_sheet[0]) / 2))] for y in range(len(paper_sheet))]
-            
-            for y in range(len(paper_sheet[0])):
-                new_y = abs(y - fold[1]) - 1
-                for x in range(len(paper_sheet)):
+            for x in range(x1, x2):
+                for y in range(fold[1] + 1, y2):
                     if paper_sheet[x][y] == '#':
-                        new_paper[x][new_y] = '#'
-
-            paper_sheet = new_paper[:]
+                        paper_sheet[x][y2 - y - 1] = '#'
+            y2 = fold[1]
 
         if first_fold_dot_count == None:    
-            first_fold_dot_count = sum([line.count('#') for line in paper_sheet])
+            first_fold_dot_count = sum([line[:y2].count('#') for line in paper_sheet[:x2]])
 
-    return first_fold_dot_count, paper_sheet
+    return first_fold_dot_count, paper_sheet, x2, y2
 
 if __name__ == "__main__":
     with open("input/day_13.txt") as file:    
@@ -52,15 +43,15 @@ if __name__ == "__main__":
                     max_y = y
             elif 'fold' in line:
                 axis, line = line.split('=')
-                folds.append([axis, int(line)])
+                folds.append([axis[-1], int(line)])
 
-        part_1_solution, part_2_solution = FoldThePaper(points, folds, max_x, max_y)
+        part_1_solution, part_2_solution, x2, y2 = FoldThePaper(points, folds, 0, max_x + 1, 0, max_y + 1)
         
         print("Part 1:", part_1_solution)
         print("Part 2:")
-        # Print code
-        for y in reversed(range(len(part_2_solution[0]))):
-            for x in reversed(range(len(part_2_solution))): 
+        # Print result of part 2
+        for y in range(y2):
+            for x in range(x2): 
                 print(part_2_solution[x][y], end='')
             print()
     
